293c440bb3a81c16a5799c231369eac171e2dbcd
Max <example@gmail.com>
2022-06-08 23:35:03 +0200

    full page (desktop)

--- js/header.js
+++ js/header.js
@@ -1,12 +1,15 @@
 const menu = () => {
 
     const header = document.querySelector('.header');
+    const menu = document.querySelector('.menu');
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 100) {
             header.style.background = "rgba(255, 255, 255, 0.9)";
+            menu.style.marginLeft = "auto";
         } else {
             header.style.background = "";
+            menu.style.marginLeft = "";
         }
     });
 
