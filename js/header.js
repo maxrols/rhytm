36ed208401617d4bd880c028094dd04314db6a19
Max <example@gmail.com>
2022-06-11 00:47:14 +0200

    responsive

--- js/header.js
+++ js/header.js
@@ -2,9 +2,13 @@ const menu = () => {
 
     const header = document.querySelector('.header');
     const menu = document.querySelector('.menu');
+    const menuList = document.querySelector('.menu__list');
+    const menuLinks = document.querySelectorAll('.menu__link');
+    const burger = document.querySelector('.burger');
+    const closeBtn = document.querySelector('.close-btn');
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 100) {
+        if (window.scrollY > 40) {
             header.style.background = "rgba(255, 255, 255, 0.9)";
             menu.style.marginLeft = "auto";
         } else {
@@ -13,6 +17,24 @@ const menu = () => {
         }
     });
 
+    burger.addEventListener('click', () => {
+        menuList.style.transform = "translateX(0)";
+    });
+
+    menuLinks.forEach(item => {
+        item.addEventListener('click', () => {
+            menuList.style.transform = "translateX(100%)";
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        menuList.style.transform = "translateX(100%)";
+    });
+    
+
 };
 
 export default menu;
+
+
+
